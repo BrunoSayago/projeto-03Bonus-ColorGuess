34c3e98d68e389a4fd8e46009df3a011f61bd061
Bruno Sayago <brunossgripp@gmail.com>
2022-07-05 17:44:55 -0300

    Finalizando requisitos

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Constantes //
+const bolas = document.getElementsByClassName('ball');
+const codigoRGB = document.getElementById('rgb-color');
+const paragrafoResultado = document.getElementById('answer');
+const botaoReset = document.getElementById('reset-game');
+const placar = document.getElementById('score');
+
+// Funções gerais //
+
 function rnd255() {
   return Math.floor(Math.random() * 256);
 }
@@ -7,10 +16,14 @@ function stringRGB() {
   return stringCor;
 }
 
-const codigoRGB = document.getElementById('rgb-color');
-codigoRGB.innerText = `(${rnd255()}, ${rnd255()}, ${rnd255()})`;
+function determinaIndexBola() {
+  const indexRND = Math.floor(Math.random() * 6);
+  return bolas[indexRND];
+}
 
-const bolas = document.getElementsByClassName('ball');
+function determinaRGBTexto() {
+  codigoRGB.innerText = `(${rnd255()}, ${rnd255()}, ${rnd255()})`;
+}
 
 function geraCorBolas() {
   for (let index = 0; index < bolas.length; index += 1) {
@@ -20,33 +33,46 @@ function geraCorBolas() {
   }
 }
 
-geraCorBolas();
-
-function determinaIndexBola() {
-  const indexRND = Math.floor(Math.random() * 6);
-  return bolas[indexRND];
-}
-
-const codigoRGBAtual = codigoRGB.innerText;
-const stringCodigoRGBAtual = `rgb${codigoRGBAtual}`;
-
 function pintaBolaCerta(codigo) {
   const bolaCerta = determinaIndexBola();
   bolaCerta.style.setProperty('background-color', codigo);
   bolaCerta.classList.add('certa');
 }
 
-pintaBolaCerta(stringCodigoRGBAtual);
-const paragrafoResultado = document.getElementById('answer');
+function atualizaPlacar() {
+  const placarAtual = parseInt(placar.innerText, 10);
+  const novoPlacar = placarAtual + 3;
+  placar.innerText = novoPlacar;
+}
 
 function selecionaBola(evento) {
   if (evento.target.classList.contains('certa')) {
     paragrafoResultado.innerText = 'Acertou!';
+    atualizaPlacar();
   } else {
     paragrafoResultado.innerText = 'Errou! Tente novamente!';
   }
 }
 
+function retornaParagrafo() {
+  paragrafoResultado.innerText = 'Escolha uma cor';
+}
+
 for (let index = 0; index < bolas.length; index += 1) {
   bolas[index].addEventListener('click', selecionaBola);
 }
+
+function carregamentoInicial() {
+  determinaRGBTexto();
+  const codigoRGBAtual = codigoRGB.innerText;
+  const stringCodigoRGBAtual = `rgb${codigoRGBAtual}`;
+  geraCorBolas();
+  pintaBolaCerta(stringCodigoRGBAtual);
+  retornaParagrafo();
+}
+
+botaoReset.addEventListener('click', carregamentoInicial);
+
+window.onload = () => {
+  carregamentoInicial();
+};
